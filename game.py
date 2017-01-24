70bd4047eeae5a0102c585584179efbb651098eb
staffgha <ghall@roundhayschool.com>
2017-01-24 20:38:04 +0000

    first commit, all files

---
+++ game.py
@@ -0,0 +1,46 @@
+from player import *
+
+class Game():
+    def __init__(self):
+        self.backgroundColor = WHITE
+        self.sprites = pygame.sprite.Group()
+        self.player = Player()
+        self.sprites.add(self.player)
+        self.controls = {
+            pygame.K_UP: self.player.up,
+            pygame.K_DOWN: self.player.down
+        }
+
+    def handleEvents(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return True
+            if event.type == pygame.KEYUP or event.type == pygame.KEYDOWN :
+                try:
+                    self.controls[event.key](event.type)
+                except:
+                    pass
+        return False
+    def update(self):
+        self.sprites.update()
+
+    def draw(self, screen):
+        screen.fill(self.backgroundColor)
+        self.sprites.draw(screen)
+        pygame.display.flip()
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game = Game()
+    done = False
+    clock = pygame.time.Clock()
+    while not done:
+        done = game.handleEvents()
+        game.update()
+        game.draw(screen)
+        clock.tick(FPS)
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ player.py
@@ -0,0 +1,31 @@
+import pygame
+from constants import *
+
+
+class Player(pygame.sprite.Sprite):
+    ya = 0
+    maxA = 10
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Graphics/PNG/playerShip1_blue.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 100
+        self.image = pygame.transform.rotate(self.image,-90)
+
+    def update(self):
+        self.rect.y += self.ya
+
+    def up(self,keytype):
+        if keytype == pygame.KEYDOWN:
+            self.ya = -1
+        else:
+            self.ya = 0
+
+    def down(self,keytype):
+        if keytype == pygame.KEYDOWN:
+            self.ya = 1
+        else:
+            self.ya = 0
+
